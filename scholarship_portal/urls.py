de05b9dd3479b9089ded8860e1ce93cdedce7980
Karl Rey <karlcolminar@gmail.com>
2026-06-03 01:43:53 +0800

    Run migrations before Railway start

--- scholarship_portal/urls.py
+++ scholarship_portal/urls.py
@@ -3,14 +3,33 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
+from django.db import connection
 
 
 def health_check(request):
+    db_status = {'connected': False}
+    try:
+        connection.ensure_connection()
+        tables = connection.introspection.table_names()
+        db_status = {
+            'connected': True,
+            'has_user_table': 'accounts_user' in tables,
+            'has_session_table': 'django_session' in tables,
+            'table_count': len(tables),
+        }
+    except Exception as exc:
+        db_status = {
+            'connected': False,
+            'error': exc.__class__.__name__,
+            'message': str(exc)[:200],
+        }
+
     return JsonResponse({
         'status': 'ok',
         'version': settings.APP_VERSION,
         'debug': settings.DEBUG,
         'axes_enabled': getattr(settings, 'AXES_ENABLED', False),
+        'database': db_status,
     })
 
 urlpatterns = [
